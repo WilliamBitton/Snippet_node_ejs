c736c8d3d67e187965349d48497e7daa739e6e3b
WilliamBitton <118488828+WilliamBitton@users.noreply.github.com>
2023-08-29 14:41:50 -0400

    commit

--- app.js
+++ app.js
@@ -1,35 +1,36 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+"use strict"
+
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
 const mongoose = require('mongoose')
 const errorController = require('./controllers/errorController')
-var indexRouter = require('./routes/index');
-var app = express();
+const indexRouter = require('./routes/index')
+const app = express()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
+app.use('/', indexRouter)
 
 app.use(errorController.getError)
 
-module.exports = app;
+module.exports = app
 
 const PORT = process.env.PORT || 3000
 const MONGOOSE = process.env.MONGOOSE
-mongoose.connect(MONGOOSE)
+mongoose.connect('mongodb+srv://Snippet:<password>@snippet.cr1qbt2.mongodb.net/snippets?retryWrites=true&w=majority')
   .then(() => {
     console.log('La connexion à la base de données est établie')
 
     app.listen(PORT, () => {
-      console.log('app.listen 3000')
+      console.log('app.listen')
     })
   })
 
